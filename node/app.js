926d982aa5f1b99c5eb98e30786dc437b9e30350
qiyuan.tang01 <qiyuan.tang01@liulishuo.com>
2020-11-06 11:16:07 +0800

    feat: format_json_by_path

--- node/app.js
+++ node/app.js
@@ -1,19 +1,7 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { format_json_by_path } = require("../pkg/ssvm_nodejs_starter_lib.js");
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
-const port = 3000;
+const path = "/workspaces/ssvm-nodejs-starter/node/2020-10-19-11-50-19.json";
 
-const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
-});
+format_json_by_path(path);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+console.log("finished!");
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,62 @@
+extern crate jsonxf;
+extern crate time;
+use std::fs::File;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+    println!("The Rust function say() received {}", s);
+    let r = String::from("hello ");
+    return r + s;
+}
+
+#[wasm_bindgen]
+pub fn format_json_by_path(path: &str) -> String {
+    println!("The path received {}", path);
+
+    match format_json(path) {
+        Err(e) => {
+            println!("format_json error {}", e);
+            return "".to_string();
+        }
+        Ok(_) => "".to_string(),
+    }
+}
+
+fn format_json(path: &str) -> Result<(), String> {
+    let indent = String::from("  ");
+
+    let mut input: Box<dyn std::io::Read> = match File::open(path) {
+        Ok(f) => Box::new(f),
+        Err(e) => {
+            let mut estr = String::from(path);
+            estr.push_str(": ");
+            estr.push_str(&e.to_string());
+            return Err(estr);
+        }
+    };
+
+    let tmp_path = path.to_string() + ".tmp";
+
+    let mut output: Box<dyn std::io::Write> = match File::create(&tmp_path) {
+        Ok(f) => Box::new(f),
+        Err(e) => {
+            let mut estr = String::from(tmp_path);
+            estr.push_str(": ");
+            estr.push_str(&e.to_string());
+            return Err(estr);
+        }
+    };
+
+    let mut xf = jsonxf::Formatter::pretty_printer();
+    xf.indent = indent;
+    let result = xf.format_stream(&mut input, &mut output);
+
+    std::fs::rename(tmp_path, path).unwrap();
+
+    match result {
+        Err(e) => Err(e.to_string()),
+        Ok(_) => Ok(()),
+    }
 }
